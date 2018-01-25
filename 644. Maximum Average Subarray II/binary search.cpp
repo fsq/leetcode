e08a1648d7c23defd3ae5d1f30581dc75ae654a8
Shuangquan Feng <fengsq@utexas.edu>
2018-01-25 13:49:23 -0600

    update binary search

--- 644. Maximum Average Subarray II/binary search.cpp
+++ 644. Maximum Average Subarray II/binary search.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    
     // https://leetcode.com/problems/maximum-average-subarray-ii/description/
-    // O(nlog(MAX-MIN)) binary search solution
-    // check existence of i,j s.t. (s[i]-s[j])/(i-j) >= mid, where i-j>=k
-    // ==> mid*j-s[j] >= mid*i-s[i]
+    // O(nlog(MAX-MIN)) binary search
     
     bool valid(const vector<int>& a, int k, double x) {
-        double pre = INT_MIN;
+        
+        // exist (s[i]-s[j])/(i-j)>=x
+        // ->    s[i]-i*x >= s[j]-j*x
+        // record the minumum s[j]-j*x
+        
+        double minsj = INT_MAX;
         for (int i=0; i<a.size(); ++i) {
             if (i-k>=0)
-                pre = max(pre, x*(i-k)-a[i-k]);
-            if (pre >= x*i-a[i]) 
+                minsj = min(minsj, a[i-k]-(i-k)*x);
+            if (a[i]-i*x>=minsj)
                 return true;
         }
         return false;
     }
     
     double findMaxAverage(vector<int>& nums, int k) {
-        vector<int> s(nums.size()+1, 0);
-        partial_sum(nums.begin(), nums.end(), s.begin()+1);
+        nums.insert(nums.begin(), 0);
+        vector<int> s(nums.size(), 0);
+        partial_sum(nums.begin(), nums.end(), s.begin());
         auto min_max = minmax_element(nums.begin(), nums.end());
         
         double l=*min_max.first, r=*min_max.second;
