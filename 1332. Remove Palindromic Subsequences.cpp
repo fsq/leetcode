766369bb07ce0d191b1640d83ae0cf07aa979d1c
Shuangquan Feng <fengsq@utexas.edu>
2020-01-27 08:17:34 -0800

    easy

---
+++ 1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if (s.empty()) return 0;
+        for (int i=0,j=s.size()-1; i<j; ++i,--j)
+            if (s[i] != s[j]) 
+                return 2;
+        return 1;
+    }
+};
