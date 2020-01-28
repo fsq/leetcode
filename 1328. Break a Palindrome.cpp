b212103c97d42073390a14f06bb9a183d70122f1
Shuangquan Feng <fengsq@utexas.edu>
2020-01-28 08:37:09 -0800

    easy

---
+++ 1328. Break a Palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        if (s.size()==1) return "";
+        for (int i=0; i<s.size()/2; ++i)
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                return s;
+            }
+        s.back() = 'b';
+        return s;
+    }
+};
