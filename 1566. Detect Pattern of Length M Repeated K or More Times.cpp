1101e054e4e2f4cba835053690cbe5b474109b6f
blackhold02 <sq_feng@foxmail.com>
2020-08-31 08:29:07 -0700

    ad hoc

--- 1566. Detect Pattern of Length M Repeated K or More Times.cpp
+++ 1566. Detect Pattern of Length M Repeated K or More Times.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    // bool containsPattern(vector<int>& a, int m, int k) {
+    //     for (int i=0; i+m*k<=a.size(); ++i) {
+    //         bool ok = true;
+    //         for (int j=0; j<m; ++j) {
+    //             int c = a[i + j];
+    //             for (int r=1; r<k; ++r) 
+    //                 if (c != a[i + r*m + j]) {
+    //                     goto last;
+    //                 }
+    //         }
+    //         if (ok) return true;
+    //         last: continue;
+    //     }
+    //     return false;
+    // }
     bool containsPattern(vector<int>& a, int m, int k) {
-        for (int i=0; i+m*k<=a.size(); ++i) {
-            bool ok = true;
-            for (int j=0; j<m; ++j) {
-                int c = a[i + j];
-                for (int r=1; r<k; ++r) 
-                    if (c != a[i + r*m + j]) {
-                        goto last;
-                    }
-            }
-            if (ok) return true;
-            last: continue;
+        int len = 0;
+        for (int i=0; i+m<a.size(); ++i) {
+            if (a[i] != a[i+m])
+                len = 0;
+            else if (++len == m*(k-1))
+                return true;
         }
         return false;
     }
