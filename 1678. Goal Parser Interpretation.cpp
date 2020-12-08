4c02dbf2b2328882af8ce7d2ebae800e9f9e3d76
blackhold02 <sq_feng@foxmail.com>
2020-12-08 09:21:00 -0800

    easy

---
+++ 1678. Goal Parser Interpretation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string interpret(string s) {
+        string ans;
+        for (int i=0; i<s.size(); )
+            if (s[i]=='G') {
+                ans += 'G';
+                ++i;
+            } else if (s[i]=='(' && s[i+1]==')') {
+                ans += 'o';
+                i += 2;
+            } else {
+                ans += "al";
+                i += 4;
+            }
+        return ans;
+    }
+};
