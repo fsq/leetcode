136ea43c8dc816e6a0faa261671dd0cb99cb11ba
blackhold02 <sq_feng@foxmail.com>
2020-12-18 09:42:15 -0800

    easy

---
+++ 1688. Count of Matches in Tournament.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans = 0;
+        while (n!=1) 
+            if (n & 1) {
+                ans += (n-1) / 2;
+                n = (n-1)/2 + 1;
+            } else {
+                ans += n / 2;
+                n >>= 1;
+            }
+        return ans;
+    }
+};
