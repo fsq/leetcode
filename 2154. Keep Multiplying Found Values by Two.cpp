5af38465283a1eb643c0a71c6d4a5e80003c2012
fsq <fengsq@utexas.edu>
2022-07-21 19:06:43 -0700

    easy

---
+++ 2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        while (st.count(original)) original *= 2;
+        return original;
+    }
+};
