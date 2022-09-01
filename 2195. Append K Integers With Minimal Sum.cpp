574255a744072ef880d45509275f829ce1742eb6
fsq <fengsq@utexas.edu>
2022-08-31 20:04:08 -0700

    fix bug

--- 2195. Append K Integers With Minimal Sum.cpp
+++ 2195. Append K Integers With Minimal Sum.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     long long minimalKSum(vector<int>& nums, int k) {
-        unordered_set<int> st(nums.begin(), nums.end());
-        int ans = 0;
-        for (int x=1,c=0; c<k; ++x)
-            if (!st.count(x)) {
-                ans += x;
-                ++c;
+        sort(nums.begin(), nums.end());
+        nums.resize(unique(nums.begin(), nums.end()) - nums.begin());
+        long long ans = 0, i = 0, x = 1;
+        while (k)
+            if (i<nums.size() && nums[i]==x) {
+                ++i;
+                ++x;
+            } else {
+                int y = i<nums.size() ? min<long long>(nums[i]-1, x+k-1) : x+k-1;
+                ans += (x+y)*(y-x+1)/2;
+                k -= y - x + 1;
+                x = y + 1;
             }
         return ans;
     }
