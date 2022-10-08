f10db7fbeb8b0acb11bf720be14fbac8dcf8257f
fsq <fengsq@utexas.edu>
2022-10-08 10:30:58 -0700

    easy

---
+++ 2240. Number of Ways to Buy Pens and Pencils.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        long long ans = 0;
+        for (int i=0; i<=total/cost1; ++i) {
+            ans += (total - i*cost1) / cost2 + 1;
+        }
+        return ans;
+    }
+};
