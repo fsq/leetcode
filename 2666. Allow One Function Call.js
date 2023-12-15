d7538c491c534922d3eee83d698058b8694fe5c9
fsq <fengsq@utexas.edu>
2023-12-14 19:42:31 -0800

    js closure

---
+++ 2666. Allow One Function Call.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+
+  return function(...args){
+        if (called)
+            return undefined;
+        else {
+            called = true;
+            return fn(...args);        
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
