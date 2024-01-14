ee3b69f97bb017b085485f03b8df40f4cae2cbce
fsq <fengsq@utexas.edu>
2024-01-14 10:47:44 -0800

    js timeout

---
+++ 2622. Cache With Time Limit.js
@@ -0,0 +1,45 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let exist = this.cache.has(key);
+    if (exist) clearTimeout(this.cache.get(key).timer);
+    this.cache.set(key, {
+        value,
+        timer:  setTimeout(() => { this.cache.delete(key);}, duration)
+    });
+    return exist;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if (this.cache.has(key)) {
+        return this.cache.get(key).value;
+    } else {
+        return -1;
+    }
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
