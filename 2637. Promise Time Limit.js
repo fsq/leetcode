7a79c4813c02f68463b180012c461efc94665ffa
fsq <fengsq@utexas.edu>
2024-10-27 09:40:01 -0700

    rename

--- 2637. Promise Time Limit.js
+++ 2637. Promise Time Limit.js
@@ -6,21 +6,15 @@
 var timeLimit = function(fn, t) {
     
     return async function(...args) {
-        let p1 = fn(...args);
-        let p2 = new Promise((resolve, reject) => {
-            setTimeout(()=>reject("Time Limit Exceeded"), t);
+        let exec = new Promise((resolve) => resolve(fn(...args)));
+
+        let ttl = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded');
+            }, t);
         });
-        return Promise.race([p1, p2]);
-        // return new Promise((resolve, reject) => {
-        //     let p1 = fn(...args);
-        //     let p2 = new Promise((resolve, reject) => {
-        //         setTimeout(()=>reject("Time Limit Exceeded"), t);
-        //     });
-        //     return Promise.race([p1, p2]).then(
-        //         (res) => resolve(res),
-        //         (err) => reject(err)
-        //     );
-        // });
+        return Promise.race([exec, ttl]);
+     
     }
 };
 
